d598954e0612b528587d353fc01e234f5a1a69a5
iojuch <petrovich8384@mail.ru>
2023-08-29 23:23:24 +0300

    change gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        int onTheAccount = 100;
+        int payment = 1100;
+        int bonus = 100;
+        int receivedBonus;
+        if (payment > 1000) {
+            receivedBonus = payment / bonus;
+        } else {
+            receivedBonus = 0;
+        }
+        int amount = onTheAccount + payment + receivedBonus;
 
-        int eaters = 5; // сколько людей будут есть
-
-        int water = 3000; // миллилитров воды
-        int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
-
-        System.out.println("Сварили суп. На одного человека вышло:");
-        System.out.println((water / eaters) + " миллилитров воды");
-        System.out.println((potatoes / eaters) + " картофелин(а)");
-        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
-        System.out.println((spices / eaters) + " ложек(ка) специй");
+        System.out.println("Сумма на счету:" + amount);
 
     }
 }
